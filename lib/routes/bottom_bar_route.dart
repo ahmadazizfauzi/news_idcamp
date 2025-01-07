95423fac703744504bc6182c0238f5dc097834ea
ahmadazizfauzi <aziz.fauzi1811@gmail.com>
2025-01-07 07:14:47 +0700

    feature/bottom_bar

---
+++ lib/routes/bottom_bar_route.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+import 'package:news/constants/color/app_color.dart';
+import 'package:news/persentation/screens/welcome/welcome_screen.dart';
+
+class BottomBarRoute extends StatefulWidget {
+  const BottomBarRoute({super.key});
+
+  @override
+  State<BottomBarRoute> createState() => _BottomBarRouteState();
+}
+
+class _BottomBarRouteState extends State<BottomBarRoute> {
+  int _selectedIndex = 0;
+
+  final List<Widget> _screens = [
+    const WelcomeScreen(),
+    const WelcomeScreen(),
+    const WelcomeScreen(),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _screens[_selectedIndex],
+      bottomNavigationBar: Padding(
+        padding: EdgeInsets.only(
+          bottom: MediaQuery.of(context).padding.bottom + 10,
+        ),
+        child: Container(
+          decoration: BoxDecoration(
+            boxShadow: [
+              BoxShadow(
+                color: Colors.grey.withOpacity(0.2),
+                spreadRadius: 5,
+                blurRadius: 10,
+              ),
+            ],
+          ),
+          child: BottomNavigationBar(
+            currentIndex: _selectedIndex,
+            onTap: _onItemTapped,
+            selectedItemColor: AppColors.primary,
+            unselectedItemColor: Colors.grey,
+            type: BottomNavigationBarType.fixed,
+            selectedFontSize: 14,
+            unselectedFontSize: 14,
+            iconSize: 28,
+            items: const [
+              BottomNavigationBarItem(
+                icon: Icon(Icons.home),
+                label: 'Home',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.compass_calibration_outlined),
+                label: 'Bot',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.person),
+                label: 'Profile',
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
